c9fc373628fc5e865b42c9158962dbb18bcaef51
drmuu <leo@leocarlsson.se>
2019-06-17 16:59:23 +0200

    Update Pollutantmean.R

--- Assignment1/Pollutantmean.R
+++ Assignment1/Pollutantmean.R
@@ -1,22 +1,12 @@
 #PollutantMean
 pollutantmean <- function(directory, pollutant, id = 1:332) {
-        ## 'directory' is a character vector of length 1 indicating the
-        ## location of the csv files
-        
-        ## 'pollutant' is a character vector of length 1 indicating the
-        ## name of the pollutant for which we will calculate the 
-        ## mean; either "sulfate" or "nitrate"
-        
-        ## 'id' is an integer vector indicating the monitor ID numbers
-        # to be used
-        
-        ## Return the mean of the pollutant across all monitors list
-        ## in the 'id' vector (ignoring NA values)
-        ## NOTE: Do not round the results!
-        file_use <- list.files(paste(getwd(),"/",directory,sep="")) #Creates an indexed list of files
+        #Creates an indexed list of files
+        file_use <- list.files(paste(getwd(),"/",directory,sep="")) 
         data_frame <- data.frame() #Creates an empty data frame
-        for (i in id) { #loop that goes through id, using it as index, and depositing file in frame
-                data_frame <- rbind(data_frame, read.csv(paste(getwd(),"/",directory,"/",file_use[i],sep = "")))         
+        for (i in id) { #loop that goes through id, using it as index,
+                        #and depositing file in frame
+                data_frame <- rbind(data_frame, read.csv(paste(getwd(),
+                                "/",directory,"/",file_use[i],sep = "")))         
         }
         mean(data_frame[[pollutant]], na.rm = T)
 }

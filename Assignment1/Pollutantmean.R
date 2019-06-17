b3ba9e20facd9ebaba37aea387ba55b51485bc56
drmuu <leo@leocarlsson.se>
2019-06-17 16:51:02 +0200

    Update Pollutantmean.R

--- Assignment1/Pollutantmean.R
+++ Assignment1/Pollutantmean.R
@@ -13,4 +13,10 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
         ## Return the mean of the pollutant across all monitors list
         ## in the 'id' vector (ignoring NA values)
         ## NOTE: Do not round the results!
+        file_use <- list.files(paste(getwd(),"/",directory,sep="")) #Creates an indexed list of files
+        data_frame <- data.frame() #Creates an empty data frame
+        for (i in id) { #loop that goes through id, using it as index, and depositing file in frame
+                data_frame <- rbind(data_frame, read.csv(paste(getwd(),"/",directory,"/",file_use[i],sep = "")))         
+        }
+        mean(data_frame[[pollutant]], na.rm = T)
 }

399d133604a0f1cd7670d8162c1af072b7ead8e9
drmuu <leo@leocarlsson.se>
2019-06-18 14:21:06 +0200

    Assignment 2

--- Assignment1/complete.R
+++ Assignment1/complete.R
@@ -1,15 +1,18 @@
 complete <- function(directory, id = 1:332) {
-        ## 'directory' is a character vector of length 1 indicating the
-        ## location of the csv files
+        file_use <- list.files(paste(getwd(),"/",directory,sep="")) 
+        data_frame <- data.frame() #Creates an empty data frame
+        complete_nobs <- data.frame() #Creates a second empty frame for depositing
+        for (i in id) { #loop that goes through id, using it as index,
+                #and depositing file in frame
+                data_frame <- read.csv(paste(getwd(),
+                                "/",directory,"/",file_use[i],sep = "")) #Read csv from directory
+                                                                        #deposit in data frame
+                good<-complete.cases(data_frame) #sort out only complete cases
+                x <- data.frame(id = i,nobs =nrow(data_frame[good,])) #write into new 
+                #data frame, with i in first position and number of rows (nobs) in second
+                complete_nobs <- rbind(complete_nobs, x) #deposit the two numbers into new frame and repeat
+        }
         
-        ## 'id' is an integer vector indicating the monitor ID numbers
-        # to be used
-        
-        ## return a data frame of the form:
-        ## id nobs
-        ## 1 117
-        ## 2 1041
-        ## ...
-        ## Where 'id' is the monitor ID number and 'nobs' is the
-        ## number of complete cases
+        complete_nobs
+       
 }
